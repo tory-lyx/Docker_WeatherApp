5adf53373cc1ae47a9444253eabab7016c12438a
tory-lyx <m.07.viktoriia@gmail.com>
2026-04-24 02:36:05 +0300

    Initial commit: weather backend
    all files from backend part for one project

---
+++ DockerWeather_WebApp/Program.cs
@@ -0,0 +1,40 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// додаємо CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// використовуємо CORS
+app.UseCors("AllowAll");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.UseHttpsRedirection(); // тільки для dev
+}
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
